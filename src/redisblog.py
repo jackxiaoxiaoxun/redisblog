f45068fa3369b298812255d54d2454e0aca5e09a
jack.xun <jkqi@sohu.com>
2016-08-04 22:32:44 +0800

    webpy run

---
+++ src/redisblog.py
@@ -0,0 +1,29 @@
+
+import web
+
+
+
+### Url mappings
+
+urls = (
+    '/', 'Index',
+    '/view/(\d+)', 'View',
+    '/new', 'New',
+    '/delete/(\d+)', 'Delete',
+    '/edit/(\d+)', 'Edit',
+)
+
+
+
+### Templates
+t_globals = {
+    'datestr': web.datestr
+}
+render = web.template.render('templates', base='base', globals=t_globals)
+
+
+
+app = web.application(urls, globals())
+
+if __name__ == '__main__':
+    app.run()
